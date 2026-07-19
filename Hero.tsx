7fd7b1c0b873db015ceeb0ea0deeadf52d64b6d9
Carlos Vega <corvpro4@gmail.com>
2026-07-19 14:21:02 -0600

    Create Hero.tsx

---
+++ Hero.tsx
@@ -0,0 +1,29 @@
+import LanguageHeader from "./LanguageHeader";
+
+export default function Hero(){
+
+return(
+
+<section className="text-center pt-24">
+
+<LanguageHeader/>
+
+<h1 className="text-6xl mt-8">
+
+Carlos Vega
+
+</h1>
+
+<p className="mt-8 text-gray-600 text-xl">
+
+Researching the intersection of mathematics,
+
+law, and economics.
+
+</p>
+
+</section>
+
+)
+
+}
