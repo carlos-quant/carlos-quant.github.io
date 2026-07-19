36e36e253e055bd738b7dabf01ca200b87f46e1d
Carlos Vega <corvpro4@gmail.com>
2026-07-19 14:19:39 -0600

    Create Navbar.tsx

---
+++ Navbar.tsx
@@ -0,0 +1,33 @@
+export default function Navbar() {
+
+return(
+
+<nav className="flex justify-between items-center py-8">
+
+<div className="text-lg font-semibold">
+
+Carlos Vega
+
+</div>
+
+<div className="flex gap-8">
+
+<a href="/">Home</a>
+
+<a href="/research">Research</a>
+
+<a href="/articles">Articles</a>
+
+<a href="/projects">Projects</a>
+
+<a href="/notes">Reading Notes</a>
+
+<a href="/about">About</a>
+
+</div>
+
+</nav>
+
+)
+
+}
